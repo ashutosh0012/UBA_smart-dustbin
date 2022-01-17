3af364c0a97120ed25e90b7abbdb677c8379fb42
ashutosh0012 <ashutosh0012@users.noreply.github.com>
2022-01-17 22:15:50 +0530

    my 1st commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = 5000
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/index.html'))
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+
+
+app.listen(port, () => {
+  console.log(`App started at http://localhost:${port}`);
+})
